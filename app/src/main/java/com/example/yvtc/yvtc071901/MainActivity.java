ce849ba7c4fb0f9f4c1c6989d9a892d5bb74a065
Shaojung <shaojung@gmail.com>
2017-07-19 09:42:57 +0800

    SimpleAdapter

--- app/src/main/java/com/example/yvtc/yvtc071901/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc071901/MainActivity.java
@@ -6,19 +6,37 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
     String cities[] = {"台北", "台中", "台南", "高雄"};
+    String codes[] = {"02", "04", "06", "07"};
+    ArrayList<HashMap<String, String>> mylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, cities);
+
+        mylist = new ArrayList<>();
+        int i;
+        for (i=0;i<=3;i++)
+        {
+            HashMap<String, String> m1 = new HashMap<>();
+            m1.put("city", cities[i]);
+            m1.put("code", codes[i]);
+            mylist.add(m1);
+        }
+
+
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, mylist, android.R.layout.simple_list_item_2,
+                new String[] {"city", "code"}, new int[] {android.R.id.text1, android.R.id.text2});
         lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
